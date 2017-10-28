fdf1acbb03ea86b836f393ad8a1d544a2f9ed993
403533049 <403533049@qq.com>
2017-10-28 10:17:35 +0800

    revise

--- calculator.py
+++ calculator.py
@@ -23,17 +23,18 @@ outputfile = args[args.index('-o') +1]
 
 class Config(object):
     def __init__(self,configfile):
-        self_config = {}
+        self._config = {}
         file1 = open(configfile)
         file2 = file1.readlines()
         for line in file2:
-            line.strip()
-            front_str = line.split('=')[0]
-            back_str = line.split('=')[1]
-            self._config[front_str] = int(back_str)
+            front_str0 = line.split('=')[0]
+            front_str = front_str0.strip()
+            back_str0 = line.split('=')[1]
+            back_str = back_str0.strip().strip('\n')
+            self._config[front_str] = float(back_str)
         file1.close()
-    def get_config(self):
-        return self._config(self)
+    def get_config(self,shebaocanshu):
+        return self._config[shebaocanshu]
 
 canshu = Config(configfile)
 
@@ -44,52 +45,53 @@ class Userdata(object):
         file4 = file3.readlines()
         for line in file4:
             front_num = line.split(',')[0]
-            sqsalary = line.split(',')[1]
-            self._userdata.append(int(sqsalary))
+            shuzi = line.split(',')[1].strip('\n')
+            self._userdata.append(int(shuzi))
         file3.close()
     def calculator(self):
         tax_rate = 0
         sskcs = 0
-        if  sqsalary < canshu.get_config('JiShuL') and sqsalary >= 0:
-            jishu = canshu.get_config('JiShuL')
-        elif  sqsalary >= canshu.get_config('JiShuL')  and sqsalary <= canshu.get_config('JiShuH'):
-            jishu = sqsalary
-        elif  sqsalary > canshu.get_config('JiShuH'):
-            jishu = canshu.get_config('JiShuH')
-        else:
-            print('ParameterError')
-        insurance = jishu * (canshu.get_config('YangLao') + canshu.get_config('YiLiao')  + canshu.get_config('ShiYe')  +  canshu.get_config('GongShang') + canshu.get_config('ShengYu') + canshu.get_config('GongJiJin'))
-        ynssde = sqsalary - insurance - 3500
+        for sqsalary in self._userdata:
+            if  sqsalary < canshu.get_config('JiShuL') and sqsalary >= 0:
+                jishu = canshu.get_config('JiShuL')
+            elif  sqsalary >= canshu.get_config('JiShuL')  and sqsalary <= canshu.get_config('JiShuH'):
+                jishu = sqsalary
+            elif  sqsalary > canshu.get_config('JiShuH'):
+                jishu = canshu.get_config('JiShuH')
+            else:
+                print('ParameterError')
+            insurance = jishu * (canshu.get_config('YangLao') + canshu.get_config('YiLiao')  + canshu.get_config('ShiYe')  +  canshu.get_config('GongShang') + canshu.get_config('ShengYu') + canshu.get_config('GongJiJin'))
+            ynssde = sqsalary - insurance - 3500
 
-        if ynssde < 1500 and ynssde > 0:
-            tax_rate = 0.03
-            sskcs = 0
-        elif ynssde >= 1500 and ynssde < 4500:
-            tax_rate = 0.1
-            sskcs = 105
-        elif ynssde >= 4500 and ynssde < 9000:
-            tax_rate = 0.2
-            sskcs = 555
-        elif ynssde >= 9000 and ynssde < 35000:
-            tax_rate = 0.25
-            sskcs = 1005
-        elif ynssde >= 35000 and ynssde < 55000:
-            tax_rate = 0.3
-            sskcs = 2755
-        elif ynssde >= 55000 and ynssde < 80000:
-            tax_rate = 0.35
-            sskcs = 5505
-        elif ynssde >= 80000:
-            tax_rate = 0.45
-            sskcs = 13505
-        else:
-            tax_rate = 0
-            sskcs = 0
+            if ynssde < 1500 and ynssde > 0:
+                tax_rate = 0.03
+                sskcs = 0
+            elif ynssde >= 1500 and ynssde < 4500:
+                tax_rate = 0.1
+                sskcs = 105
+            elif ynssde >= 4500 and ynssde < 9000:
+                tax_rate = 0.2
+                sskcs = 555
+            elif ynssde >= 9000 and ynssde < 35000:
+                tax_rate = 0.25
+                sskcs = 1005
+            elif ynssde >= 35000 and ynssde < 55000:
+                tax_rate = 0.3
+                sskcs = 2755
+            elif ynssde >= 55000 and ynssde < 80000:
+                tax_rate = 0.35
+                sskcs = 5505
+            elif ynssde >= 80000:
+                tax_rate = 0.45
+                sskcs = 13505
+            else:
+                tax_rate = 0
+                sskcs = 0
 
-        tax = ynssde * tax_rate - sskcs
-        shsalary = format(sqsalary - insurance - tax,'0.2f')
-        geshui = format(tax, '0.2f')
-        shebao = format(insurance,'0.2f')
+            tax = ynssde * tax_rate - sskcs
+            shsalary = format(sqsalary - insurance - tax,'0.2f')
+            geshui = format(tax, '0.2f')
+            shebao = format(insurance,'0.2f')
 
     def dumptofile(self, outputfile):
         with open(outputfile, 'a') as file:
@@ -99,6 +101,6 @@ class Userdata(object):
             outputfile.write(str(geshui) + ',')
             outputfile.write(str(shsalary)) 
 
-finalfile = Userdate(user.csv)
+finalfile = Userdata(userdatafile)
 finalfile.calculator()
-finalfile.dumptofile(gongzi.csv)
+finalfile.dumptofile(outputfile)
